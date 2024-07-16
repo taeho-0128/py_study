71e521a0daae3218c48b378ff385fefc28ee28c6
taeho <jeff0128@naver.com>
2024-07-16 21:02:11 +0900

    변경

--- 4장/Cold Compress.py
+++ 4장/Cold Compress.py
@@ -1,3 +1,24 @@
 #Cold Compress
 #ccc19j3
 
+messages_count = int(input())
+
+while messages_count > 0:
+    messages_count -= 1
+    message = input()
+    thisword = 0
+    nextword= thisword + 1
+    word_count = 1
+
+    while thisword < len(message):
+        if nextword < len(message) and message[thisword] == message[nextword]:
+            word_count += 1
+        else:
+            print(word_count, message[thisword], end=' ')
+            word_count = 1
+        thisword += 1
+    print()
+
+
+
+
