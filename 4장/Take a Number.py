e4e08be00ab7c9e0de476d2dc121f7e1ccc84f40
taeho <jeff0128@naver.com>
2024-07-16 21:56:02 +0900

    변경

--- 4장/Take a Number.py
+++ 4장/Take a Number.py
@@ -1,2 +1,24 @@
-#Take a Number
-#ecoo13r1p1
+tomorrow_startnum = int(input())
+
+limit = 1000000
+TAKE_count = 0
+SERVE_count = 0
+
+while limit > 0:
+    limit -= 1
+    status = input()
+
+    if status == 'TAKE':
+        TAKE_count += 1
+        tomorrow_startnum += 1
+        if tomorrow_startnum > 999:
+            tomorrow_startnum = 1
+    elif status == 'SERVE':
+        SERVE_count += 1
+    elif status == "CLOSE":
+        waiting_students = TAKE_count - SERVE_count
+        print(f"{TAKE_count} {waiting_students} {tomorrow_startnum}")
+        TAKE_count = 0
+        SERVE_count = 0
+    elif status == "EOF":
+        break
